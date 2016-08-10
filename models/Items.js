b082ae51950aade00ebd2f96390371235e305435
jaredcsullivan <sullivanjc91@gmail.com>
2016-08-09 21:28:02 -0500

    Delete Items.js

--- models/Items.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var ItemSchema = mongoose.Schema({
-	itemName: String,
-	author: String,
-	todoId: { type: mongoose.Schema.Types.ObjectId, ref: 'Todo'}
-});
-
-mongoose.model('Item', ItemSchema);
