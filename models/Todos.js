e1994fb5d97924d7585cd4811ea43b07307a33c7
jaredcsullivan <sullivanjc91@gmail.com>
2016-08-09 21:28:06 -0500

    Delete Todos.js

--- models/Todos.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var TodoSchema = new mongoose.Schema({
-  todoName: String,
-  author : String,
-  items: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item' }]
-});
-
-mongoose.model('Todo', TodoSchema);
